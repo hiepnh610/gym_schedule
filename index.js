27c8a75938ae7b89f7844886e296f156b7175f3d
Hiep.Nguyen <hiepnh610@gmail.com>
2017-12-28 11:31:05 +0700

    add more package to project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
 const mongoose   = require('mongoose');
+const morgan     = require('morgan');
 
 const config = require('./config');
 
@@ -10,6 +11,7 @@ const plan = require('./api/plan');
 
 mongoose.connect(config.mongoUri, { useMongoClient: true });
 
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
