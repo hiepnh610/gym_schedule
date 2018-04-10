706282ef678ac250bf515f06ed8f181cc33e2c39
Hiep Nguyen <hiep@mcb.vn>
2018-04-10 09:55:18 +0700

    add created by field into workout day model

--- model/workout_day.js
+++ model/workout_day.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
+const Plan = require('./plan');
+
 const WorkoutDaySchema = new Schema({
+    created_by: {
+        type: Schema.Types.ObjectId,
+        ref: 'Plan'
+    },
     name: {
         required: true,
         type: String
