b8619b8686aac89ab35c7c9a937f89987e74bea5
Hiep Nguyen <hiep@mcb.vn>
2018-04-12 09:47:59 +0700

    create app workout list

---
+++ vue/src/components/workout/AppListWorkout.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row" v-if="getListWorkout.length > 0">
+    <div class="col-xs-12 col-md-8 col-md-offset-2">
+      <table class="table table-striped table-bordered table-hover list-plans text-center">
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>week day</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="plan in getListWorkout">
+            <td>
+              <router-link :to="'workout/' + plan._id" class="text-primary text-capitalize" @click.native="updateNamePlan(plan.name)">{{ plan.name }}</router-link>
+            </td>
+
+            <td>{{ plan.week_day }}</td>
+
+            <td>
+              <a class="btn btn-sm btn-warning m-r-5">Edit</a>
+
+              <a class="btn btn-sm btn-danger">Delete</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AppListWorkout',
+  components: {},
+  data () {
+    return {
+    }
+  },
+  computed: {
+    getListWorkout () {
+      return this.$store.getters.listPlans
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- vue/src/components/workout/AppWorkOut.vue
+++ vue/src/components/workout/AppWorkOut.vue
@@ -2,6 +2,8 @@
   <div class="text-center">
     <h1 class="text-center">{{ namePlanProp }}</h1>
 
+    <app-list-workout></app-list-workout>
+
     <a class="btn btn-md btn-primary" @click.prevent="createWorkout">
       <i class="fa fa-fw fa-plus-circle" aria-hidden="true"></i>
       Create Workout Day
@@ -12,11 +14,15 @@
 </template>
 
 <script>
+import axios from 'axios'
+import config from '@/config'
+
 import AppWorkoutCreate from './AppWorkoutCreate.vue'
+import AppListWorkout from './AppListWorkout.vue'
 
 export default {
   name: 'AppWorkout',
-  components: { AppWorkoutCreate },
+  components: { AppWorkoutCreate, AppListWorkout },
   data () {
     return {
       namePlanProp: ''
@@ -30,6 +36,22 @@ export default {
   },
   created () {
     this.namePlanProp = this.$store.getters.namePlanProp
+
+    axios.get(config.domainAddress + config.api.workout, {
+      params: {
+        id: this.$route.params.id
+      }
+    })
+    .then(function (response) {
+      this.$store.dispatch('setListPlans', response.data)
+    }.bind(this))
+    .catch(function (error) {
+      if (error.response && error.response.data && error.response.data.message) {
+        this.errContent = error.response.data.message
+      } else {
+        this.errContent = 'Error happened.'
+      }
+    }.bind(this))
   }
 }
 </script>
