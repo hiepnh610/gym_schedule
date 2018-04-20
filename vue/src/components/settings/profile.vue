5d90703dd9204b99dc92dc481167cb5a1bf1a197
Hiep Nguyen <hiepnh610@gmail.com>
2018-04-20 12:19:56 +0700

    change spacing and column

--- vue/src/components/settings/profile.vue
+++ vue/src/components/settings/profile.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12 col-md-8 offset-md-2">
+      <div class="col-12 col-md-6 offset-md-3">
         <div class="profile">
-          <h2>Personal Profile</h2>
+          <h2 class="text-center mb-5">Personal Profile</h2>
 
           <form>
             <div class="form-group">
