5ee0694e33d97d987de1c114b42610ce6fb7d4d6
Hiep Nguyen <hiepnh610@gmail.com>
2018-05-20 21:20:03 +0700

    auto create images folder

--- node/api/upload.js
+++ node/api/upload.js
@@ -1,15 +1,21 @@
 const multer = require('multer');
 const path   = require('path');
+const mkdirp = require('mkdirp');
+
+const uploadPath = 'images';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public/images');
+        cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 });
+
 const upload = multer({ storage: storage }).single('avatar');
+
+mkdirp.sync(uploadPath);
 
 const uploadImage = (req, res) => {
     upload(req, res, (err) => {
