e355e1e90b915e77fb0aeacf2a40d86ab1636dfc
Hiep Nguyen <hiep@mcb.vn>
2018-05-21 09:36:32 +0700

    handle upload image on the front end

--- vue-app/src/config.js
+++ vue-app/src/config.js
@@ -2,15 +2,16 @@
 
 const domainAddress = 'http://localhost:3000'
 const api = {
+  avatar: '/api/avatar/',
   listPlans: '/api/list_plans',
   listWorkout: '/api/list_workout',
   login: '/api/login',
   modifyPassword: '/api/modify_password/',
   plans: '/api/plans/',
   sign_up: '/api/sign_up',
+  upload: '/api/upload',
   user: '/api/user/',
-  workout: '/api/workout_day/',
-  avatar: '/api/avatar/'
+  workout: '/api/workout_day/'
 }
 
 module.exports = { api, domainAddress }
