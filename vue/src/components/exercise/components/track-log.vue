e972497f768df5139c5bf9def98f7c9c6e1271f2
Hiep Nguyen <hiepnh610@gmail.com>
2018-10-24 22:51:04 +0700

    split track log code to new component

---
+++ vue/src/components/exercise/components/track-log.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="track-log">
+    <div class="track-log-header">
+      <div class="row justify-content-between">
+        <div class="col-12 col-md-4">
+          <a href="#" @click.prevent="addMoreSet">
+            Add more set
+
+            <font-awesome-icon icon="plus-circle" />
+          </a>
+        </div>
+
+        <div class="col-12 col-md-4">
+          <div class="select-custom">
+            <font-awesome-icon icon="caret-down" />
+
+            <select class="form-control">
+              <option value="" disabled selected>Status</option>
+              <option value="Not Started">Not Started</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Done">Done</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="track-log-content">
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th class="text-nowrap">2 sets</th>
+            <th>Weight</th>
+            <th>Reps</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+
+            <td>
+              <input type="text" class="form-control">
+            </td>
+
+            <td>
+              <input type="text" class="form-control">
+            </td>
+
+            <td>
+              <font-awesome-icon icon="times-circle" class="text-danger" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
+import { State, Action, Getter } from 'vuex-class'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import axios from 'axios'
+
+@Component({
+  components: {
+  FontAwesomeIcon
+  },
+  })
+export default class TrackLog extends Vue {
+  setNumber: number = 2
+
+  addMoreSet () {}
+
+  removeSet () {}
+}
+</script>
--- vue/src/components/exercise/exercise-update.vue
+++ vue/src/components/exercise/exercise-update.vue
@@ -16,99 +16,19 @@
           <b-card no-body>
             <b-tabs card>
               <b-tab title="Track Log" active>
-                <div class="track-log">
-                  <div class="track-log-header">
-                    <div class="row justify-content-between">
-                      <div class="col-12 col-md-4">
-                        <a href="#">
-                          Add more set
-
-                          <font-awesome-icon icon="plus-circle" />
-                        </a>
-                      </div>
-
-                      <div class="col-12 col-md-4">
-                        <div class="select-custom">
-                          <font-awesome-icon icon="caret-down" />
-
-                          <select class="form-control">
-                            <option value="" disabled selected>Status</option>
-                            <option value="Not Started">Not Started</option>
-                            <option value="In Progress">In Progress</option>
-                            <option value="Done">Done</option>
-                          </select>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="track-log-content">
-                    <table class="table">
-                      <thead class="thead-light">
-                        <tr>
-                          <th class="text-nowrap">2 sets</th>
-                          <th>Weight</th>
-                          <th>Reps</th>
-                          <th></th>
-                        </tr>
-                      </thead>
-
-                      <tbody>
-                        <tr>
-                          <td>1</td>
-
-                          <td>
-                            <input type="text" class="form-control">
-                          </td>
-
-                          <td>
-                            <input type="text" class="form-control">
-                          </td>
-
-                          <td>
-                            <font-awesome-icon icon="times-circle" class="text-danger" />
-                          </td>
-                        </tr>
-
-                        <tr>
-                          <td>2</td>
-
-                          <td>
-                            <input type="text" class="form-control">
-                          </td>
-
-                          <td>
-                            <input type="text" class="form-control">
-                          </td>
-
-                          <td>
-                            <font-awesome-icon icon="times-circle" class="text-danger" />
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-
-                  <div class="form-group form-button text-center mb-0">
-                    <button class="btn btn-sm btn-primary">
-                      Update
-                      <font-awesome-icon icon="spinner" spin v-if="loading" />
-                    </button>
-
-                    <button class="btn btn-sm btn-secondary">Cancel</button>
-                  </div>
-                </div>
-              </b-tab>
-
-              <b-tab title="History">
-                Impedit odit dolore a, mollitia voluptate deserunt culpa aspernatur rem facere, nisi, natus maiores ad aut nam. Laudantium provident fugit quibusdam eius?
-              </b-tab>
-
-              <b-tab title="Note">
-                Laudantium provident fugit quibusdam eius?
+                <track-log />
               </b-tab>
             </b-tabs>
           </b-card>
+
+          <div class="form-group form-button text-center mb-0">
+            <button class="btn btn-sm btn-primary">
+              Update
+              <font-awesome-icon icon="spinner" spin v-if="loading" />
+            </button>
+
+            <button class="btn btn-sm btn-secondary">Cancel</button>
+          </div>
         </div>
       </div>
     </div>
@@ -124,11 +44,14 @@ import axios from 'axios'
 import config from '@/config'
 import { Response } from '@/util'
 
+import trackLog from './components/track-log.vue'
+
 const namespaceModal: string = 'modal'
 
 @Component({
   components: {
-  FontAwesomeIcon
+  FontAwesomeIcon,
+  trackLog
   },
   })
 export default class ExerciseUpdate extends Vue {
