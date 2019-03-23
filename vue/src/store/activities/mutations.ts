a769753327724c00e09a20f09e6be37c4f39be8d
Hiep.Nguyen <hiepnh610@gmail.com>
2019-03-23 12:58:52 +0700

    update new logic for remove activity

--- vue/src/store/activities/mutations.ts
+++ vue/src/store/activities/mutations.ts
@@ -7,16 +7,20 @@ export const mutations: MutationTree<ActivityState> = {
   },
 
   deleteActivity (state: any, id: string): void {
-    for (const key of state.listActivities) {
-      if (state.listActivities[key]._id === id) {
-        state.listActivities.splice(key, 1)
-      }
+    for (const key in state.listActivities) {
+      if (state.listActivities.hasOwnProperty(key)) {
+        if (state.listActivities[key]._id === id) {
+          state.listActivities.splice(key, 1)
+        }
 
-      // This loop for find id of activity
-      // Because the data of profile activities has different structure
-      for (const key2 of state.listActivities[key]) {
-        if (state.listActivities[key][key2]._id === id) {
-          state.listActivities[key].splice(key2, 1)
+        // This loop for find id of activity
+        // Because the data of profile activities has different structure
+        for (const key2 in state.listActivities[key]) {
+          if (state.listActivities.hasOwnProperty(key)) {
+            if (state.listActivities[key][key2]._id === id) {
+              state.listActivities[key].splice(key2, 1)
+            }
+          }
         }
       }
     }
